f9dce3bbbff5c66b2e307255a4fc6e3a32d7ce6e
Aubriot Marc <marc.aubriot@outlook.fr>
2024-01-11 10:52:35 +0100

    class init

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+/* 
+ * Class Joueur
+ * Class Bouton
+ * Class Dashboard
+*/
+
+// initialisation du jeu
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
