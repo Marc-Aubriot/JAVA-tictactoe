aa165824df2c6cec1ddd3135afe6740116bacd08
Aubriot Marc <marc.aubriot@outlook.fr>
2024-01-11 21:11:58 +0100

    method pour dessiner le plateau (+solution)

--- Main.java
+++ Main.java
@@ -1,21 +1,9 @@
-
-import java.awt.*;
-import java.awt.event.*;
-import java.util.*;
-import javax.swing.*;
-
-/* 
- * Class Joueur
- * Class Bouton
- * Class Dashboard
-*/
-
-// initialisation du jeu
 public class Main {
 
-    public static void main(String[] args) {
-	// je suis un commentaire c'est blako qui l'a écrit 
-	// Afficher "Bonjour" à l'écran
+    static void methodBlako() {
+        // je suis un commentaire c'est blako qui l'a écrit 
+
+	    // Afficher "Bonjour" à l'écran
         System.out.println("Bonjour");
 
         // Créer un tableau de fruits
@@ -25,9 +13,11 @@ public class Main {
         System.out.println("Liste de fruits :");
         for (String fruit : fruits) {
             System.out.println(fruit);
-	}
+	    }
+    }
+
+    public static void main(String[] args) {
+        //methodBlako();
+        Game tictactoe = new Game();
     }
 }
-
-
-
